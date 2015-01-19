48194fcb0b8ed4fde51b3b764a5f4b293421c6fb
Bryan Larsen <bryan@larsen.st>
2015-01-19 15:47:18 -0500

    webpack build for bower
    `npm run build` will now create lib/ReactRouterBootstrap.js and
    lib/ReactRouterBootstrap.min.js.  according to your CONTRIBUTING.md you
    don't want these files in the PR, so make sure you add them when you
    merge this PR.
    
    A starter bower.json is included so you can upload to bower.   I have
    not registered the react-router-bootstrap name.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+var plugins = [
+];
+
+if (process.env.COMPRESS) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+module.exports = {
+
+  output: {
+    library: 'ReactRouterBootstrap',
+    libraryTarget: 'var'
+  },
+
+  externals: {
+    react: 'React',
+    'react/addons': 'React',
+//    'react/lib/cx': 'React.addons.classSet',
+    'react/lib/Object.assign': 'React.__spread',
+    'react-router': 'ReactRouter',
+    'react-bootstrap': 'ReactBootstrap',
+    'react-bootstrap/Button': 'ReactBootstrap.Button',
+    'react-bootstrap/MenuItem': 'ReactBootstrap.MenuItem',
+    'react-bootstrap/NavItem': 'ReactBootstrap.NavItem',
+  },
+
+  node: {
+    buffer: false
+  },
+
+  plugins: plugins
+};
