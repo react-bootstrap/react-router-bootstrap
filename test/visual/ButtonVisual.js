351416cafe07a4628041309313adb46e86116ba3
Lionel Ringenbach <Lionel Ringenbach>
2016-06-08 10:35:44 +0200

    Updated visual test to use ButtonLink

--- test/visual/ButtonVisual.js
+++ test/visual/ButtonVisual.js
@@ -3,7 +3,7 @@ import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import Button from 'react-bootstrap/lib/Button';
 import { Link } from 'react-router';
 
-import LinkContainer from '../../src/LinkContainer';
+import ButtonLink from '../../src/ButtonLink';
 
 export default () => (
   <div>
@@ -20,26 +20,14 @@ export default () => (
       <Button bsStyle="link">Link</Button>
     </ButtonToolbar>
 
-    <h3>LinkContainer</h3>
+    <h3>ButtonLink</h3>
     <ButtonToolbar>
-      <LinkContainer to="/home">
-        <Button>Default</Button>
-      </LinkContainer>
-      <LinkContainer to="/home">
-        <Button bsStyle="success">Success</Button>
-      </LinkContainer>
-      <LinkContainer to="/home">
-        <Button bsStyle="info">Info</Button>
-      </LinkContainer>
-      <LinkContainer to="/home">
-        <Button bsStyle="warning">Warning</Button>
-      </LinkContainer>
-      <LinkContainer to="/home">
-        <Button bsStyle="danger">Danger</Button>
-      </LinkContainer>
-      <LinkContainer to="/home">
-        <Button bsStyle="link">Link</Button>
-      </LinkContainer>
+      <ButtonLink to="/home">Default</ButtonLink>
+      <ButtonLink to="/home" bsStyle="success">Success</ButtonLink>
+      <ButtonLink to="/home" bsStyle="info">Info</ButtonLink>
+      <ButtonLink to="/home" bsStyle="warning">Warning</ButtonLink>
+      <ButtonLink to="/home" bsStyle="danger">Danger</ButtonLink>
+      <ButtonLink to="/home" bsStyle="link">Link</ButtonLink>
     </ButtonToolbar>
   </div>
 );
