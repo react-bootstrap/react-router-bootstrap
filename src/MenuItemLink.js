6efce56d450468324e1d4f9bf7d53b0325aa4781
Lionel Ringenbach <Lionel Ringenbach>
2016-06-08 10:37:33 +0200

    Added MenuItem wrapper

---
+++ src/MenuItemLink.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MenuItem } from 'react-bootstrap';
+
+import LinkContainer from './LinkContainer';
+
+const propTypes = {
+  children: React.PropTypes.node,
+};
+
+// Don't use a stateless function, to allow users to set a ref.
+/* eslint-disable react/prefer-stateless-function */
+class MenuItemLink extends React.Component {
+  render() {
+    return (
+      <LinkContainer {...this.props}>
+        <MenuItem>
+          {this.props.children}
+        </MenuItem>
+      </LinkContainer>
+    );
+  }
+}
+/* eslint-enable react/prefer-stateless-function */
+
+MenuItemLink.propTypes = propTypes
+
+export default MenuItemLink;
