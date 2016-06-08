1be93d25baa75b0625288e36d7e0557d98a9e9f9
Lionel Ringenbach <Lionel Ringenbach>
2016-06-08 10:36:40 +0200

    Added NavItemLink wrapper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export IndexLinkContainer from './IndexLinkContainer';
 export LinkContainer from './LinkContainer';
 export ButtonLink from './ButtonLink';
+export NavItemLink from './NavItemLink';
