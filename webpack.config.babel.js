fc4800f883bc8bbe260fec1424a46de3816096d6
jochenberger <fooberger@gmail.com>
2017-09-01 09:37:46 +0200

    Fix webpack config
    In #201, the switch to `react-router-dom` was made but the webpack config was not updated. Therefore, the current UMD bundles include `react-router-dom` and depend on `react-router`.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -32,11 +32,11 @@ export default {
       },
     },
     {
-      'react-router': {
-        root: 'ReactRouter',
-        commonjs2: 'react-router',
-        commonjs: 'react-router',
-        amd: 'react-router',
+      'react-router-dom': {
+        root: 'ReactRouterDOM',
+        commonjs2: 'react-router-dom',
+        commonjs: 'react-router-dom',
+        amd: 'react-router-dom',
       },
     },
   ],
