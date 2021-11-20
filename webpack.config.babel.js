c56705a9c59ca8aa861239227d9ed76eef66b4b3
kyletsang <6854874+kyletsang@users.noreply.github.com>
2021-11-19 17:22:25 -0800

    chore(ci): fix build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,52 +1,47 @@
 const path = require('path');
-const webpack = require('webpack');
-const yargs = require('yargs');
 
-const { optimizeMinimize } = yargs.alias('p', 'optimize-minimize').argv;
-const nodeEnv = optimizeMinimize ? 'production' : 'development';
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const optimizationMinimize = argv.optimizationMinimize;
 
-module.exports = {
-  entry: {
-    ReactRouterBootstrap: './src/index.js',
-  },
+  return {
+    mode,
+    entry: {
+      ReactRouterBootstrap: './src/index.js',
+    },
 
-  output: {
-    path: path.join(__dirname, 'lib'),
-    filename: optimizeMinimize ? '[name].min.js' : '[name].js',
-    library: 'ReactRouterBootstrap',
-    libraryTarget: 'umd',
-  },
+    output: {
+      path: path.join(__dirname, 'lib'),
+      filename: optimizationMinimize ? '[name].min.js' : '[name].js',
+      library: 'ReactRouterBootstrap',
+      libraryTarget: 'umd',
+    },
 
-  module: {
-    rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+    module: {
+      rules: [
+        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      ],
+    },
+
+    externals: [
+      {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react',
+        },
+      },
+      {
+        'react-router-dom': {
+          root: 'ReactRouterDOM',
+          commonjs2: 'react-router-dom',
+          commonjs: 'react-router-dom',
+          amd: 'react-router-dom',
+        },
+      },
     ],
-  },
 
-  externals: [
-    {
-      react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react',
-      },
-    },
-    {
-      'react-router-dom': {
-        root: 'ReactRouterDOM',
-        commonjs2: 'react-router-dom',
-        commonjs: 'react-router-dom',
-        amd: 'react-router-dom',
-      },
-    },
-  ],
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
-    }),
-  ],
-
-  devtool: optimizeMinimize ? 'source-map' : false,
+    devtool: optimizationMinimize ? 'source-map' : false,
+  };
 };
--- webpack.visual.config.babel.js
+++ webpack.visual.config.babel.js
@@ -2,13 +2,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './test/visual',
-
+  mode: 'development',
   module: {
     rules: [
-      { test: /\.js/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.woff|\.woff2$/, loader: 'url?prefix=font/&limit=5000' },
-      { test: /\.eot$|\.ttf$|\.svg$/, loader: 'file?prefix=font/' },
+      { test: /\.js/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.woff|\.woff2$/, use: 'url?prefix=font/&limit=5000' },
+      { test: /\.eot$|\.ttf$|\.svg$/, use: 'file?prefix=font/' },
     ],
   },
 
